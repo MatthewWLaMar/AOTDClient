23e73eb9ecd6e5268776e67bdefe21dd5e73fb31
MatthewWLaMar <84155160+MatthewWLaMar@users.noreply.github.com>
2021-10-30 16:39:45 -0400

    set up login

---
+++ src/Register/Login.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import {Input, Form, Label, Button} from 'reactstrap'
+// import styled from "styled-components"
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { username: "", password: "" }
+    }
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    componentDidMount() {
+
+        fetch('http://localhost:3000/user/login',{
+            method: 'POST',
+            body: JSON.stringify({user:{username: this.state.username, password: this.state.password}}),
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            })
+        })
+        .then((response) => response.json())
+        .then((data) => { 
+            this.setState(data.sessionToken)
+            this.setState(data.ID)
+            console.log(data)
+        }).catch(err => {
+            alert('failed to login')
+            console.log(err)
+        })
+
+    }
+    render() { 
+        return ( 
+        <div>
+            <Form onSubmit={this.componentDidMount}>
+                    <Label htmlFor="email">Email</Label>
+                    <Input onChange={(e) => this.handleChange(e)} name="username" value={this.state.username}  placeholder="Username" type="text"/>
+                    {/* <br /> */}
+                    <Label htmlFor="password">Password</Label>
+                    <Input onChange={(e) => this.handleChange(e)} name="password" value={this.state.password} placeholder="Password" type="text"/>
+                    {/* <br /> */}
+                    <Button type="submit">Login</Button>
+                    <br />
+                    {/* <p>Don't have an account? <a href="/signup" onClick={props.switchToSignup}>Signup</a></p> */}
+
+            </Form>
+        </div> );
+    }
+}
+ 
+export default Login;
